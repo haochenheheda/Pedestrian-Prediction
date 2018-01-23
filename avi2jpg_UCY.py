3916346ee4eef31cb8e41a3541d4d68d7bfe7c7a
haochen wang <13021200713@163.com>
2018-01-23 18:57:35 +0800

    Add files via upload

---
+++ avi2jpg_UCY.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8 :
+############################################
+#haochen.wang
+#2018.01.23
+############################################
+import multiprocessing
+import time
+# import pylab
+import imageio
+import skimage
+from PIL import Image
+import os
+
+video_path = ['students003.avi','crowds_zara01.avi','crowds_zara02.avi','Arxiepiskopi_flock.avi']
+for video in video_path:
+    vid = imageio.get_reader(video,  'ffmpeg')
+    save_path = video.split('.')[0]
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    for i, img in enumerate(vid):
+                    opencvImg = skimage.img_as_ubyte(img, True)
+                    # 下边就回到熟悉的opencv套路上了
+                    img = Image.fromarray(opencvImg)
+                    img.save(save_path+'/'+str(i).zfill(6)+'.jpg')
+                    if i%1000 == 0:
+                        print('now its video:'+video+'    frame:'+str(i))
+    
